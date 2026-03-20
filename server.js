5ae17c309f4524cc19e88b740dcd39e3e626afdd
Mathilde-gzn <mathildegazezian@gmail.com>
2026-03-20 14:46:18 +0100

    fix route index

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.post('/generate', async (req, res) => {
   const { product, category, langs, modules } = req.body;
